41a3c1b507ff502b86d240c8e8424fe8a02b3ee6
VASUDHA JAGARLAMUDY <jagarlamudy1991@gmail.com>
2018-03-21 08:28:26 -0500

    Thymeleaf has been added but utext hasn't been given

--- src/main/java/com/vasudha/topic/TopicService.java
+++ src/main/java/com/vasudha/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.vasudha.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,12 +9,47 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "springmvc", "springdescriptiomn"),
+	private List<Topic> topics = new ArrayList<>( Arrays.asList(new Topic("spring", "springmvc", "springdescriptiomn"),
 			new Topic("acbd", "efgh", "ijklmnop"),
-			new Topic("124456", "ghdye57hj", "235ghgjhgdfgdgd"));
+			new Topic("124456", "ghdye57hj", "235ghgjhgdfgdgd")));
 	
 	public List<Topic> getAllTopics(){
 		
 		return topics;
 	}
+	
+	public Topic getTopic(String id){
+		
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		// TODO Auto-generated method stub
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		
+		for(int i = 0; i < topics.size(); i++){
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)){
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id, Topic topic) {
+//		for(int i = 0; i < topics.size(); i++){
+//			Topic t = topics.get(i);
+//			if(t.getId().equals(id)){
+//				topics.remove(i);
+//				return;
+//			}
+//		}
+		
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+	
 }
